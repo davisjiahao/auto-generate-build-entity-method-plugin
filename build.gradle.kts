9b565d7a55939a755cec71040df06b1289c2dfff
daviswujiahao <daviswujiahao@didiglobal.com>
2022-11-20 00:01:18 +0800

    add descript of create method intention

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,7 +44,6 @@ kotlin {
 intellij {
     pluginName.set(properties("pluginName"))
     version.set(properties("platformVersion"))
-    type.set(properties("platformType"))
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins.set(properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty))
